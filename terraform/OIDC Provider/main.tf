2a0bb8834faabd26ec011f0f641a3d35d7308067
James <sandford.james@gmail.com>
2023-05-27 21:30:09 +0100

    feat: more defaults

--- terraform/OIDC Provider/main.tf
+++ terraform/OIDC Provider/main.tf
@@ -2,27 +2,9 @@ resource "vault_identity_oidc_assignment" "aws" {
   name       = "aws-assignment"
   group_ids  = [var.aws_Group_id]
 }
-resource "vault_identity_oidc_key" "aws" {
-  name               = "aws"
-  allowed_client_ids = ["*"]
-  rotation_period    = 3600
-  verification_ttl   = 3600
-}
-
-resource "vault_identity_oidc_provider" "aws" {
-  name = "aws"
-  https_enabled = true
-  issuer_host = "https://vault.devcastops.com:8200"
-  allowed_client_ids = [
-    vault_identity_oidc_client.aws.client_id
-  ]
-  scopes_supported = [
-  ]
-}
 
 resource "vault_identity_oidc_client" "aws" {
   name          = "application"
-  key           = vault_identity_oidc_key.aws.name
   client_type   = "public"
   redirect_uris = [
     "http://127.0.0.1:9200/v1/auth-methods/oidc:authenticate:callback",
