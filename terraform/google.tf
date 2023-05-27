9e7deeef2960bb874257f5deb8f4021d5b356dd4
James <sandford.james@gmail.com>
2023-05-27 16:56:41 +0100

    feat: adding default role

--- terraform/google.tf
+++ terraform/google.tf
@@ -5,6 +5,7 @@ resource "vault_jwt_auth_backend" "google" {
     oidc_discovery_url  = "https://accounts.google.com"
     oidc_client_id      = var.google_client_id
     oidc_client_secret  = var.google_secret
+    default_role        = "default"
 }
 
 resource "vault_jwt_auth_backend_role" "google" {
